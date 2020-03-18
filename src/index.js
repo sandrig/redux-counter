5f5307dcb2cdb4fc85097393b681de9af38b00e9
sandrig <sandrig.dev@gmail.com>
2020-03-18 19:05:50 +0300

    feat: add decrement and increment handlers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import './styles.css'
 
 const counter = document.getElementById('counter')
+const addBtn = document.getElementById('add')
+const subBtn = document.getElementById('sub')
 
 let state = 0
 
 function render() {
   counter.textContent = state.toString()
 }
+
+addBtn.addEventListener('click', () => {
+  state++
+  render()
+})
+
+subBtn.addEventListener('click', () => {
+  state--
+  render()
+})
 
 render()
