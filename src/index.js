5a021cb1c27da9a42607115e49630a9d305e70bc
sandrig <sandrig.dev@gmail.com>
2020-03-19 19:01:29 +0300

    feat: add async handler

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './styles.css'
 const counter = document.getElementById('counter')
 const addBtn = document.getElementById('add')
 const subBtn = document.getElementById('sub')
+const asyncBtn = document.getElementById('async')
 
 let state = 0
 
@@ -18,6 +19,13 @@ addBtn.addEventListener('click', () => {
 subBtn.addEventListener('click', () => {
   state--
   render()
+})
+
+asyncBtn.addEventListener('click', () => {
+  setTimeout(() => {
+    state++
+    render()
+  }, 2000)
 })
 
 render()
