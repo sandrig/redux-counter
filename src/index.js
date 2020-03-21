ac8fe301df591d7b337812b308b6477694a2472f
sandrig <sandrig.dev@gmail.com>
2020-03-21 12:00:24 +0300

    refactor: add constant

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createStore } from 'redux'
 import { rootReducer } from './redux/rootReducer'
+import { INCREMENT, DECREMENT } from './redux/types'
 import './styles.css'
 
 const counter = document.getElementById('counter')
@@ -11,11 +12,11 @@ const themeBtn = document.getElementById('theme')
 const store = createStore(rootReducer, 0)
 
 addBtn.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' })
+  store.dispatch({ type: INCREMENT })
 })
 
 subBtn.addEventListener('click', () => {
-  store.dispatch({ type: 'DECREMENT' })
+  store.dispatch({ type: DECREMENT })
 })
 
 asyncBtn.addEventListener('click', () => {
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,9 @@
+import { INCREMENT, DECREMENT } from './types'
+
 export function rootReducer(state, action) {
-  if (action.type === 'INCREMENT') {
+  if (action.type === INCREMENT) {
     return state + 1
-  } else if (action.type === 'DECREMENT') {
+  } else if (action.type === DECREMENT) {
     return state - 1
   }
 
